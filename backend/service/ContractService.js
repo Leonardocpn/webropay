f914bbae4467a315630539ec9bfa2732af077d67
Leonardocpn <leonardocpn@gmail.com>
2020-07-13 09:51:57 -0300

    feat: added get,post an put method for contract

--- backend/service/ContractService.js
+++ backend/service/ContractService.js
@@ -1,5 +1,6 @@
-'use strict';
-
+"use strict";
+const db = require("../config/database");
+const { respondWithCode } = require("../utils/writer");
 
 /**
  * Get a list of all contracts
@@ -7,71 +8,90 @@
  *
  * returns List
  **/
-exports.getContracts = function() {
-  return new Promise(function(resolve, reject) {
-    var examples = {};
-    examples['application/json'] = [ {
-  "description" : "Details about a contract",
-  "id" : "123456",
-  "status" : true
-}, {
-  "description" : "Details about a contract",
-  "id" : "123456",
-  "status" : true
-} ];
-    if (Object.keys(examples).length > 0) {
-      resolve(examples[Object.keys(examples)[0]]);
-    } else {
-      resolve();
-    }
+exports.getContracts = function () {
+  return new Promise(function (resolve, reject) {
+    db.query("SELECT * FROM contracts ORDER BY id ASC")
+      .then((res) => {
+        return resolve(res.rows);
+      })
+      .catch((err) => {
+        const response = respondWithCode(500, {
+          error: "Error when retrieving data from database",
+        });
+        return reject(response);
+      });
   });
-}
-
+};
 
 /**
  * Create a new contract
- * Create a new contract in the database informing description in body
+ * Create a new contract in the database passing description and status in body
  *
- * body PostContract 
+ * body PostContract
  * returns PostContractResponse
  **/
-exports.postContract = function(body) {
-  return new Promise(function(resolve, reject) {
-    var examples = {};
-    examples['application/json'] = {
-  "description" : "New contract",
-  "id" : "123456",
-  "status" : true
-};
-    if (Object.keys(examples).length > 0) {
-      resolve(examples[Object.keys(examples)[0]]);
-    } else {
-      resolve();
+exports.postContract = function (body) {
+  return new Promise(function (resolve, reject) {
+    console.log(typeof body.status);
+    if (!body || !body.description) {
+      const response = respondWithCode(400, {
+        message: "Invalid Contract, inform description",
+      });
+      return reject(response);
     }
-  });
-}
 
+    db.query(
+      "INSERT INTO contracts (description, status) VALUES ($1, $2) RETURNING *",
+      [body.description, true]
+    )
+      .then((res) => {
+        console.log(res);
+        return resolve(res.rows);
+      })
+      .catch((err) => {
+        const response = respondWithCode(500, {
+          message: "Error when saving data in database",
+        });
+        return reject(response);
+      });
+  });
+};
 
 /**
- * Update Contract Status
- * Update a contract status with an id in path
+ * Change the status of a contract
+ * Update status of a contract with an id, it will invert the current status
  *
- * id BigDecimal id of contract that need to be updated, example 123456
+ * id BigDecimal id of contract that need to be updated
  * returns EditContractResponse
  **/
-exports.updateContract = function(id) {
-  return new Promise(function(resolve, reject) {
-    var examples = {};
-    examples['application/json'] = {
-  "description" : "New contract",
-  "id" : "123456",
-  "status" : false
-};
-    if (Object.keys(examples).length > 0) {
-      resolve(examples[Object.keys(examples)[0]]);
-    } else {
-      resolve();
+exports.updateContract = function (id) {
+  return new Promise(function (resolve, reject) {
+    if (!id || Number(id) === NaN) {
+      const response = respondWithCode(400, {
+        message: "Inform a valid contract id in path",
+      });
+      return reject(response);
     }
-  });
-}
 
+    db.query(
+      "UPDATE contracts SET status = NOT status WHERE id = $1 RETURNING *",
+      [id]
+    )
+      .then((res) => {
+        console.log(res.rows);
+        if (res.rows.length === 0) {
+          const response = respondWithCode(404, {
+            message: "Contract not found",
+          });
+          return reject(response);
+        }
+        resolve(res.rows);
+      })
+      .catch((res) => {
+        const response = respondWithCode(500, {
+          message: "Error in database when update a contract",
+        });
+        return reject(response);
+      });
+  });
+};
